dec6ff169d21ececc63f9c6e237e8267c051df5e
Mitch Laber <labermt@gmail.com>
2018-08-20 00:57:48 -0700

    Added stub fragments for draw and analyze.

--- feature/src/main/java/com/bessermt/trisolve/feature/MainActivity.java
+++ feature/src/main/java/com/bessermt/trisolve/feature/MainActivity.java
@@ -1,6 +1,13 @@
 package com.bessermt.trisolve.feature;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,21 +17,46 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements DrawFragment.OnFragmentInteractionListener,
+        AnalyzeFragment.OnFragmentInteractionListener {
+
+    private static final String TAG = "MainActivity";
+
+    private ViewPager viewPager_;
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
 
+    private void setupViewPager(ViewPager viewPager)
+    {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new DrawFragment(), "Draw");
+        adapter.addFragment(new AnalyzeFragment(), "Analyze");
+        viewPager.setAdapter(adapter);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        // Set up the ViewPager with the sections adapter.
+        viewPager_ = findViewById(R.id.container);
+        setupViewPager(viewPager_);
+
+        final TabLayout tabLayout = findViewById(R.id.tabs);
+
+        tabLayout.setupWithViewPager(viewPager_);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,9 +65,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+//        // Example of a call to a native method
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
     }
 
     @Override
@@ -65,4 +97,68 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        // TODO: Remove this and the
+        // implements DrawFragment.OnFragmentInteractionListener,
+        // AnalyzeFragment.OnFragmentInteractionListener
+        // if this is never used.
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        // TODO: watch https://www.youtube.com/watch?v=bNpWGI_hGGg
+
+        private final List<Fragment> fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitleList = new ArrayList<>();
+
+        public SectionsPagerAdapter(final FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(final Fragment fragment, final String title)
+        {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            CharSequence result = null;
+            if (position < fragmentTitleList.size()) {
+                result = fragmentTitleList.get(position);
+            }
+            else {
+                Log.e(TAG, "getPageTitle(int) domain error.");
+            }
+
+            return result;
+        }
+
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            // return PlaceholderFragment.newInstance(position + 1);
+
+            Fragment result = null;
+            if (position < fragmentList.size()) {
+                result = fragmentList.get(position);
+            }
+            else {
+                Log.e(TAG, "getItem(int) domain error.");
+            }
+
+            return result;
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+    }
 }
