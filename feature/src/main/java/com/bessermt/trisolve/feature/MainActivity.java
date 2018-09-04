901dec0e46959fbd782c6faa3969cc7815f5a69b
Mitch Laber <labermt@gmail.com>
2018-09-03 19:00:14 -0700

    Improved tab icon appearance, including highlight on selection.

--- feature/src/main/java/com/bessermt/trisolve/feature/MainActivity.java
+++ feature/src/main/java/com/bessermt/trisolve/feature/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bessermt.trisolve.feature;
 
+import android.content.res.ColorStateList;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -63,11 +64,63 @@ public class MainActivity extends AppCompatActivity
         setupViewPager(viewPager_);
 
         final TabLayout tabLayout = findViewById(R.id.tabs);
+        final int nTabs = tabLayout.getTabCount();
 
         tabLayout.setupWithViewPager(viewPager_);
 
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_draw_24dp);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_analyze_24dp);
+
+        viewPager_.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            /**
+             * This method will be invoked when the current page is scrolled, either as part
+             * of a programmatically initiated smooth scroll or a user initiated touch scroll.
+             *
+             * @param position             Position index of the first page currently being displayed.
+             *                             Page position+1 will be visible if positionOffset is nonzero.
+             * @param positionOffset       Value from [0, 1) indicating the offset from the page at position.
+             * @param positionOffsetPixels Value in pixels indicating the offset from position.
+             */
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            /**
+             * This method will be invoked when a new page becomes selected. Animation is not
+             * necessarily complete.
+             *
+             * @param position Position index of the new selected page.
+             */
+            @Override
+            public void onPageSelected(int position) {
+                final int unselected_opacity = 0xB3;
+                final int selected_opacity = 0xFF;
+
+                for (int i=0; i!=nTabs; ++i) {
+                    int opacity = unselected_opacity;
+                    if (i == position) {
+                        opacity = selected_opacity;
+                    }
+                    tabLayout.getTabAt(i).getIcon().setAlpha(opacity);
+                }
+            }
+
+            /**
+             * Called when the scroll state changes. Useful for discovering when the user
+             * begins dragging, when the pager is automatically settling to the current page,
+             * or when it is fully stopped/idle.
+             *
+             * @param state The new scroll state.
+             * @see ViewPager#SCROLL_STATE_IDLE
+             * @see ViewPager#SCROLL_STATE_DRAGGING
+             * @see ViewPager#SCROLL_STATE_SETTLING
+             */
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         // https://stackoverflow.com/questions/34562117/how-do-i-change-the-color-of-icon-of-the-selected-tab-of-tablayout
 
